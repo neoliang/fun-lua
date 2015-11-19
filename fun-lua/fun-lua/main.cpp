b0a0736640110f5fa59445d741763f0a50681bb8
neo <liangneo@qq.com>
2015-11-19 13:46:57 +0800

    init

---
+++ fun-lua/fun-lua/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fun-lua
+//
+//  Created by neo on 15-11-19.
+//  Copyright (c) 2015年 user. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
